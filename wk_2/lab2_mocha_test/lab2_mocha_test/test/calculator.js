85b6b707c25a54727abe00aed25174a7c73f5703
gokulJinu01 <gokuljinusclmda@gmail.com>
2025-01-23 12:45:44 -0500

    Lab-2 Mocha Test

---
+++ wk_2/lab2_mocha_test/lab2_mocha_test/test/calculator.js
@@ -0,0 +1,44 @@
+const { expect } = require('chai');
+const calculator = require('../app/calculator');
+
+describe('Calculator Tests', () => {
+  describe('Addition Tests', () => {
+    it('add(5, 2) should return 7 (PASS)', () => {
+      expect(calculator.add(5, 2)).to.equal(7);
+    });
+
+    it('add(5, 2) should return 8 (FAIL)', () => {
+      expect(calculator.add(5, 2)).to.equal(8);
+    });
+  });
+
+  describe('Subtraction Tests', () => {
+    it('sub(5, 2) should return 3 (PASS)', () => {
+      expect(calculator.sub(5, 2)).to.equal(3);
+    });
+
+    it('sub(5, 2) should return 5 (FAIL)', () => {
+      expect(calculator.sub(5, 2)).to.equal(5);
+    });
+  });
+
+  describe('Multiplication Tests', () => {
+    it('mul(5, 2) should return 10 (PASS)', () => {
+      expect(calculator.mul(5, 2)).to.equal(10);
+    });
+
+    it('mul(5, 2) should return 12 (FAIL)', () => {
+      expect(calculator.mul(5, 2)).to.equal(12);
+    });
+  });
+
+  describe('Division Tests', () => {
+    it('div(10, 2) should return 5 (PASS)', () => {
+      expect(calculator.div(10, 2)).to.equal(5);
+    });
+
+    it('div(10, 2) should return 2 (FAIL)', () => {
+      expect(calculator.div(10, 2)).to.equal(2);
+    });
+  });
+});
